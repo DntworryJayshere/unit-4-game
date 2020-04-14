417b2db3def965cebc621b1f4142f61a57d0de7b
Jay Rodriguez <jared.m.rodriguez@gmail.com>
2020-04-14 16:38:14 -0500

    updates to js file for crystal collector

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,139 +1,144 @@
-$(document).ready(function () {
+// GLOBAL VARIABLES
+// =================================================================
 
-    //Global variables
-    //====================================================================================== 
-
-    //score variables
-    var ranNum;
-    var totalScore;
-
-    //crystal variables
-    var gem = {
-        rubyNum: {
-            name: "ruby",
-            value: 0
-        },
-        diamondNum: {
-            name: "diamond",
-            value: 0
-        },
-        emeraldNum: {
-            name: "emerald",
-            value: 0
-        },
-        saffireNum: {
-            name: "saffire",
-            value: 0
-        }
+// Crystal Variables
+var crystal = {
+    blue:
+    {
+      name: "Blue",
+      value: 0
+    },
+    green:
+    {
+      name: "Green",
+      value: 0
+    },
+    red:
+    {
+      name: "Red",
+      value: 0
+    },
+    yellow:
+    {
+      name: "Yellow",
+      value: 0
     }
-
-    // total win and losses
-    var winsCounter = 0;
-    var lossesCounter = 0;
-
-    //functions
-    //====================================================================================== 
-
-    //function to generate random numbers
-    var generateRandom = function (min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    };
-
-    //function to start and restart the game
-    function startGame() {
-
-        //sets a new random number goal
-        ranNum = generateRandom(19, 120);
-
-        //resets the total score to zero
-        totalScore = 0;
-
-        //sets a new random number for all of the crystals
-        gem.rubyNum.value = generateRandom(1, 12);
-        gem.diamondNum.value = generateRandom(1, 12);
-        gem.emeraldNum.value = generateRandom(1, 12);
-        gem.saffireNum.value = generateRandom(1, 12);
-
-        //change the html to reflect these changes
-        $("#random-number").text(ranNum);
-        $("#total-score").text(totalScore);
-
-        //push the random number to the total score & display random number selected
-        console.log("ran" + ranNum);
-        console.log("ruby" + gem.rubyNum.value);
-        console.log("diamon" + gem.diamondNum.value);
-        console.log("emerald" + gem.emeraldNum.value);
-        console.log("saffire" + gem.saffireNum.value);
-
+  };
+  
+  // Scores (Current and Target)
+  var currentScore = 0;
+  var targetScore = 0;
+  
+  // Wins and Losses
+  var winCount = 0;
+  var lossCount = 0;
+  
+  
+  // FUNCTIONS
+  // =================================================================
+  
+  // Helper Function for getting random numbers
+  var getRandom = function(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+  
+  // Starts the Game (and restarts the game)
+  var startGame = function() {
+  
+    // Reset the Current Score
+    currentScore = 0;
+  
+    // Set a new Target Score (between 19 and 120)
+    targetScore = getRandom(19, 120);
+  
+    // Set different values for each of the crystals (between 1 and 12)
+    crystal.blue.value = getRandom(1, 12);
+    crystal.red.value = getRandom(1, 12);
+    crystal.green.value = getRandom(1, 12);
+    crystal.yellow.value = getRandom(1, 12);
+  
+    // Change the HTML to reflect all of these changes
+    $("#your-score").text(currentScore);
+    $("#target-score").text(targetScore);
+  
+  
+    // Testing Console
+    console.log("-----------------------------------");
+    console.log("Target Score: " + targetScore);
+    console.log("Blue: " + crystal.blue.value + " | Green: " + crystal.green.value + " | Red: " + crystal.red.value +
+      " | Yellow: " + crystal.yellow.value);
+    console.log("-----------------------------------");
+  };
+  
+  // Check if User Won or Lost and Reset the Game
+  var checkWin = function() {
+  
+    // Check if currentScore is larger than targetScore
+    if (currentScore > targetScore) {
+      alert("Sorry. You lost!");
+      console.log("You Lost");
+  
+      // Add to Loss Counter
+      lossCount++;
+  
+      // Change Loss Count HTML
+      $("#loss-count").text(lossCount);
+  
+      // Restart the game
+      startGame();
     }
-
-    var checkWin = function () {
-
-        // Check if totalScore is larger than randomNum
-        if (totalScore > ranNum) {
-            alert("Sorry. You lost!");
-            console.log("You Lost");
-
-            // Add to Loss Counter
-            lossesCounter++;
-
-            // Change Loss Count HTML
-            $("#losses").text(lossesCounter);
-
-            // Restart the game
-            startGame();
-        }
-
-        else if (totalScore === ranNum) {
-            alert("Congratulations! You Won!");
-            console.log("You Won!");
-
-            // Add to the Win Counter
-            winsCounter++;
-
-            // Change Win Count HTML
-            $("#wins").text(winsCounter);
-
-            // Restart the game
-            startGame();
-        }
-    };
-
-    var addValues = function (clickedGem) {
-
-        totalScore += clickedgem.value;
-        $("#total-score").text(totalScore)
-
-        checkWin();
-
-        console.log("total Score: " + totalScore);
-    };
-
-    // MAIN PROCESS
-    // =================================================================
-
-    // Starts the Game the First Time.
-
-    $("#start-game").click(function () {
-        startGame();
-        //add css to #start-game display = none; display show at the end of the game boolean
-        console.log("game is starting")
-    })
-
-    $("#ruby-image").click(function () {
-        addValues(gem.rubyNum);
-    });
-
-    $("#diamond-image").click(function () {
-        addValues(gem.diamondNum);
-    });
-
-    $("#emerald-image").click(function () {
-        addValues(gem.emeraldNum);
-    });
-
-    $("#saffire-image").click(function () {
-        addValues(gem.saffireNum);
-    });
-
-})
+  
+    else if (currentScore === targetScore) {
+      alert("Congratulations! You Won!");
+      console.log("You Won!");
+  
+      // Add to the Win Counter
+      winCount++;
+  
+      // Change Win Count HTML
+      $("#win-count").text(winCount);
+  
+      // Restart the game
+      startGame();
+    }
+  
+  };
+  
+  // Respond to clicks on the crystals
+  var addValues = function(clickedCrystal) {
+  
+    // Change currentScore
+    currentScore += clickedCrystal.value;
+  
+    // Change the HTML to reflect changes in currentScore
+    $("#your-score").text(currentScore);
+  
+    // Call the checkWin Function
+    checkWin();
+  
+    // Testing Console
+    console.log("Your Score: " + currentScore);
+  };
+  
+  // MAIN PROCESS
+  // =================================================================
+  
+  // Starts the Game the First Time.
+  startGame();
+  
+  $("#blue").click(function() {
+    addValues(crystal.blue);
+  });
+  
+  $("#red").click(function() {
+    addValues(crystal.red);
+  });
+  
+  $("#green").click(function() {
+    addValues(crystal.green);
+  });
+  
+  $("#yellow").click(function() {
+    addValues(crystal.yellow);
+  });
+  
